5e51b29816a7afd5a7c4034c29def92e609f6e9a
srsirius <32639033+srsirius@users.noreply.github.com>
2020-12-23 09:34:06 +0700

    Delete backend.tf

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-    backend "s3" {
-        profile                 = "sirius-netology"
-        bucket                  = "sirius-bucket"
-        dynamodb_table          = "app-state"
-        region                  = "us-east-1"
-        key                     = "app-state/terraform.tfstate"
-        }
-}
